cc0e53b17825c4551ddec75150beed2e3020946b
Tristan Ross <tristan.ross@midstall.com>
2023-05-31 17:23:43 -0700

    chore: add padding to privacy view

--- lib/views/privacy.dart
+++ lib/views/privacy.dart
@@ -4,9 +4,32 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:markdown/markdown.dart' hide Text;
+import 'package:url_launcher/url_launcher_string.dart';
+
+class _MarkdownConstantPadding extends MarkdownPaddingBuilder {
+  _MarkdownConstantPadding({ required this.padding }) : super();
+
+  final EdgeInsets padding;
+
+  @override
+  EdgeInsets getPadding() => padding;
+}
 
 class Privacy extends StatelessWidget {
   const Privacy({ super.key });
+
+  void _handleError(BuildContext context, Object e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(AppLocalizations.of(context)!.genericErrorMessage(e.toString())),
+          duration: const Duration(milliseconds: 1500),
+          behavior: SnackBarBehavior.floating,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        )
+    );
+  }
 
   @override
   Widget build(BuildContext context) =>
@@ -41,6 +64,16 @@ class Privacy extends StatelessWidget {
                   data: snapshot.data!,
                   extensionSet: ExtensionSet.gitHubFlavored,
                   styleSheetTheme: MarkdownStyleSheetBaseTheme.material,
+                  paddingBuilders: {
+                    "h1": _MarkdownConstantPadding(padding: const EdgeInsets.all(8.0)),
+                    "h2": _MarkdownConstantPadding(padding: const EdgeInsets.all(8.0)),
+                    "p": _MarkdownConstantPadding(padding: const EdgeInsets.all(8.0)),
+                  },
+                  onTapLink: (text, href, title) {
+                    if (href != null) {
+                      launchUrlString(href!).catchError((error) => _handleError(context, error));
+                    }
+                  },
                 );
               }
               return const CircularProgressIndicator();
